1c39cf394933c3075116da4e2e13b7896fda7815
fa7ehak <fa7ehak@gmail.com>
2022-12-07 21:24:34 +0530

    Update lint-staged config to fix eslint and stylelint autofixable issues

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,10 +1,10 @@
 const prettierCmd = 'prettier --find-config-path --cache --cache-strategy content --write';
-const stylelintCmd = 'stylelint --aei --cache --cache-strategy content';
-const eslintCmd = 'eslint --max-warnings=0 --cache --cache-strategy content';
+const stylelintCmd = 'stylelint --aei --cache --cache-strategy content --fix';
+const eslintCmd = 'eslint --max-warnings=0 --cache --cache-strategy content --fix';
 
 module.exports = {
-  '**/*.js': [prettierCmd, eslintCmd],
-  '**/*.html': [prettierCmd, stylelintCmd, eslintCmd],
-  '**/*.css': [prettierCmd, stylelintCmd, eslintCmd],
-  '**/*.{md,json}': [prettierCmd],
+  '**/*.js': [prettierCmd, eslintCmd, 'git add'],
+  '**/*.html': [prettierCmd, stylelintCmd, eslintCmd, 'git add'],
+  '**/*.css': [prettierCmd, stylelintCmd, 'git add'],
+  '**/*.{md,json}': [prettierCmd, 'git add'],
 };

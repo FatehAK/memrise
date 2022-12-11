fc49f8d53b910bb672a7f7fac6a0cef3e89eef08
fa7ehak <fa7ehak@gmail.com>
2022-12-11 14:28:52 +0530

    build: 📦️ Add image minifier during build

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { resolve } from 'path';
+import viteImagemin from 'vite-plugin-imagemin';
 import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 
@@ -15,6 +16,32 @@ export default defineConfig({
     open: true,
     port: 3000,
   },
+  plugins: [
+    viteImagemin({
+      gifsicle: true,
+      optipng: false,
+      jpegTran: false,
+      svgo: {
+        multipass: true,
+        plugins: [
+          {
+            name: 'preset-default',
+            params: {
+              overrides: {
+                removeViewBox: false, // https://github.com/svg/svgo/issues/1128
+                addAttributesToSVGElement: {
+                  params: {
+                    attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
+                  },
+                },
+              },
+            },
+          },
+          'sortAttrs',
+        ],
+      },
+    }),
+  ],
   build: {
     rollupOptions: {
       plugins: [
